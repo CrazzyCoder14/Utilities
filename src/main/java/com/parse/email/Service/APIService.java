f5935e758d5975581e5afff227c6376bfe3edbb4
ankit <ankit@ankit-VivoBook-15-ASUS-Laptop-X507UF>
2019-10-26 19:48:02 +0530

    config file-1

--- src/main/java/com/parse/email/Service/APIService.java
+++ src/main/java/com/parse/email/Service/APIService.java
@@ -1,6 +1,9 @@
 package com.parse.email.Service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface APIService {
 	public void runDocker(String type);
-	
+	public int createCSVConfig(String name) throws IOException;
 }
--- src/main/java/com/parse/email/Service/APIServiceImpl.java
+++ src/main/java/com/parse/email/Service/APIServiceImpl.java
@@ -1,12 +1,69 @@
 package com.parse.email.Service;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class APIServiceImpl implements APIService {
 
 	@Override
 	public void runDocker(String type) {
 		// TODO Auto-generated method stub
+		Process p;
+		try {
+			p=Runtime.getRuntime().exec("sudo docker-compose up");
+			p.waitFor();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
 		
+	}
+
+	@Override
+	public int createCSVConfig(String name) throws IOException {
+		// TODO Auto-generated method stub
+		String homeDir = System.getProperty("user.home");
+		String dest=homeDir+"/Utilities/CSV/csv_conf.conf";
+		File file = new File(dest);
+		if (file.createNewFile())
+		{
+		    System.out.println("File is created!");
+		} else {
+		    System.out.println("File already exists.");
+		}
+		FileWriter writer = new FileWriter(file);
+		String str = "input {\n" + 
+				"	file {\n" + 
+				"		path => \"/usr/share/input/"+name+".csv\"\n" + 
+				"		sincedb_path => \"/dev/null\"\n" + 
+				"		start_position => \"beginning\"\n" + 
+				"	}\n" + 
+				"}\n" + 
+				"\n" + 
+				"filter {\n" + 
+				"    csv {\n" + 
+				"        columns => [\n" + 
+				"            \"Id\",\n" + 
+				"            \"Name\",\n" + 
+				"	    \"Subject\"\n" + 
+				"        ]\n" + 
+				"    }\n" + 
+				"}\n" + 
+				"\n" + 
+				"output {\n" + 
+				"  elasticsearch {\n" + 
+				"    hosts => \"elasticsearch:9200\"\n" + 
+				"    index => \"sample\"\n" + 
+				"    document_type => \"temp\"\n" + 
+				"    document_id => \"%{Id}\" \n" + 
+				"  }  \n" + 
+				"}";
+		writer.write(str);
 		
+		return 0;
 	}
 
 }
